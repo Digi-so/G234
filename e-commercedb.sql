9f75bd782099fbc860f9cd683d0464d6971f4ff6
Nicole Apono <napononicole@gmail.com>
2025-04-24 20:17:31 +0300

    creating colors table first since it has no foreign keys

--- e-commercedb.sql
+++ e-commercedb.sql
@@ -3,3 +3,10 @@ CREATE DATABASE IF NOT EXISTS e_commerce_db;
 
 -- Use the database
 USE database_name;
+
+-- Create table for color
+CREATE TABLE color (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    hex_code VARCHAR(7)
+);
